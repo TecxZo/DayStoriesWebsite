f4156cc15c3f42488b81679fe42b1121b6da5ee0
pavants777 <pavangowdats10@gmail.com>
2025-09-25 22:00:05 +0530

    update hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivacyPolicyDayStories from './pages/privacyPage';
 import Home from './pages/homePage';
 import DataDeletionPolicyDayStories from './pages/dataDeletion';
